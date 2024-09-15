fc4e7468bd303818e698590b8fb2d88eac15f11d
BillOfBalrog <boaz.teller@gmail.com>
2024-09-16 00:44:42 +0300

    Add initial AppHeader component layout

---
+++ cmps/AppHeader.jsx
@@ -0,0 +1,18 @@
+
+export function AppHeader() {
+
+    return (
+        <header className="app-header full main-layout">
+            <section>
+                <h1>MissBooks</h1>
+
+                <nav className="app-nav">
+                    <a>Home</a>
+                    <a>About</a>
+                    <a>Books</a>
+                </nav>
+            </section>
+        </header>
+    )
+
+}
