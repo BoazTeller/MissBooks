3649afb38a4be90158a6d3c11ee73170ebb742d4
BillOfBalrog <boaz.teller@gmail.com>
2024-09-16 00:49:30 +0300

    Add initial RootCmp layout

---
+++ RootCmp.jsx
@@ -0,0 +1,23 @@
+import { AppHeader } from './cmps/AppHeader.jsx'
+import { HomePage } from './pages/HomePage.jsx'
+import { BookIndex } from './pages/BookIndex.jsx'
+import { AboutUs } from './pages/AboutUs.jsx'
+
+const { useState } = React
+
+export function App() {
+
+    const [page, setPage] = useState('home')
+
+    return (
+        <section className="app">
+            <AppHeader />
+
+            <main className="container main-layout">
+                {page === 'home' && <HomePage />}    
+                {page === 'about' && <AboutUs />}  
+                {page === 'book' && <BookIndex />}    
+            </main>
+        </section>
+    )
+}
