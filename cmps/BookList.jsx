b43ef5adcf1c388a442ff45af70f2c4035ec93ea
BillOfBalrog <boaz.teller@gmail.com>
2024-09-16 07:51:04 +0300

    Add onSelectBook handler to BookList for detailed view

--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -1,12 +1,15 @@
 import { BookPreview } from "./BookPreview.jsx"
 
-export function BookList({ books }) {
+export function BookList({ books, onSelectBook }) {
 
     return (
         <ul className="book-list container">
             {books.map((book) =>
                 <li key={book.id}>
                     <BookPreview key={book.id} book={book} />
+                    <section className="book-actions">
+                        <button onClick={() => onSelectBook(book.id)}>Details</button>
+                    </section>
                 </li>
             )}
         </ul>
