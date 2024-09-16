fc2eadb3c6fca2ab15da8367e4345a84925151a3
BillOfBalrog <boaz.teller@gmail.com>
2024-09-16 12:02:02 +0300

    Add BookEdit feature to enable editing books in BookIndex

--- pages/BookIndex.jsx
+++ pages/BookIndex.jsx
@@ -1,13 +1,17 @@
+// Added to test BookEdit
+
 import { bookService } from "../services/book.service.js"
 import { BookList } from "../cmps/BookList.jsx"
 import { BookFilter } from "../cmps/BookFilter.jsx"
 import { BookDetails } from "./BookDetails.jsx"
+import { BookEdit } from "./BookEdit.jsx"
 
 const { useState, useEffect } = React
 
 export function BookIndex() {
 
     const [books, setBooks] = useState(null)
+    const [isEdit, setIsEdit] = useState(false)
     const [selectedBookId, setSelectedBookId] = useState(null)
     const [filterBy, setFilterBy] = useState(bookService.getDefaultFilter())
 
@@ -31,15 +35,34 @@ export function BookIndex() {
         setSelectedBookId(bookId)
     }
 
+    function onSaveBook(bookToSave) {
+        bookService.save(bookToSave)
+            .then(() => {
+                setIsEdit(false)
+                setSelectedBookId(null)
+                loadBooks()
+            })
+            .catch(err => {
+                console.log('Had issues with book save:', err)
+            })
+    }
+
     if (!books) return <h1>Loading...</h1>
 
     return (
         <section className="book-index">
-            {selectedBookId 
-                ? <BookDetails 
-                      bookId={selectedBookId}
-                      onBack={() => setSelectedBookId(null)}
-                  />
+            {selectedBookId
+                ? isEdit
+                      ? <BookEdit
+                            bookId={selectedBookId}
+                            onSaveBook={onSaveBook}
+                            onCancel={() => setIsEdit(false)}
+                        /> 
+                      : <BookDetails 
+                            bookId={selectedBookId}
+                            onBack={() => setSelectedBookId(null)}
+                            onEdit={() => setIsEdit(true)}
+                        />
                 : <React.Fragment>
                       <h2>Book Index</h2>
                       <BookFilter 
