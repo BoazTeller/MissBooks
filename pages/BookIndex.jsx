61b687d836b27d9feeb9864e1de183677afbfff3
BillOfBalrog <boaz.teller@gmail.com>
2024-09-16 03:19:40 +0300

    Implement basic book loading and display functionality in BookIndex

--- pages/BookIndex.jsx
+++ pages/BookIndex.jsx
@@ -1,10 +1,32 @@
+import { bookService } from "../services/book.service.js"
+import { BookList } from "../cmps/BookList.jsx"
 
 const { useState, useEffect } = React
 
 export function BookIndex() {
+
+    const [books, setBooks] = useState(null)
+
+    useEffect(() => {
+        loadBooks()
+    }, [])
+
+    function loadBooks() {
+        bookService.query()
+            .then(books => setBooks(books))
+            .catch(err => {
+                console.error('Failed to query books:', err)
+            })
+    }
+
+    if (!books) return <h1>Loading...</h1>
+
     return (
         <section>
             <h2>Book Index</h2>
+            <BookList
+                books={books}
+            />
         </section>
     )
 }
