9ea54c557780fddec9e595973dcd6b7e14f66a74
BillOfBalrog <boaz.teller@gmail.com>
2024-09-22 21:58:19 +0300

    Add fallback image for BookPreview on error

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -1,9 +1,13 @@
 export function BookPreview({ book }) {
 
+    function getDefaultUrl(ev) {
+        ev.target.src = 'https://via.placeholder.com/150'
+    }
+
     return (
         <article className="book-preview">
             <h3>{book.title}</h3>
-            <img src={book.thumbnail} alt={book.title} />
+            <img src={book.thumbnail} alt={book.title} onError={getDefaultUrl} />
             <p>{book.description}</p>
             <p>Price: {book.listPrice.amount} {book.listPrice.currencyCode}</p>
             {book.isOnSale && <p>On Sale!</p>}
