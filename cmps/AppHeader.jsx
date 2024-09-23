ac0c634c72d6c8f29be2f8879d1c8a58f5d00d94
BillOfBalrog <boaz.teller@gmail.com>
2024-09-24 02:32:00 +0300

    Refactor AppHeader to use React Router for navigation

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,25 +1,35 @@
-export function AppHeader({ onSetPage }) {
+const { useNavigate } = ReactRouter
+const { NavLink } = ReactRouterDOM
+
+export function AppHeader() {
+
+    const navigate = useNavigate()
+
     const navLinks = [
-        { title: 'Home', page: 'home' },
-        { title: 'About', page: 'about' },
-        { title: 'Books', page: 'book' }
+        { title: 'Home', path: '' },
+        { title: 'About', path: 'about' },
+        { title: 'Books', path: 'book' }
     ]
+
+    function onNavigateHome() {
+        navigate('/')
+    }
 
     return (
         <header className="app-header full main-layout">
             <section>
-                <h1>MissBooks</h1>
+                <h1 onClick={onNavigateHome}>MissBooks</h1>
 
                 <nav className="app-nav">
-                    {navLinks.map(navLink =>
-                        <a 
-                            key={navLink.title}
-                            onClick={(ev) => onSetPage(ev, navLink.page)}
+                    {navLinks.map(navLink => 
+                        <NavLink
+                            key={navLink.path}
+                            to={`/${navLink.path}`}
                             className="nav-link"
                             title={`Go to ${navLink.title}`}
                         >
-                            {navLink.title}    
-                        </a>
+                            {navLink.title}
+                        </NavLink>
                     )}
                 </nav>
             </section>
