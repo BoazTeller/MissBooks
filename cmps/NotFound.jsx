d3a0d32c85af1adca0945aa7290fb1bc60a5373c
BillOfBalrog <boaz.teller@gmail.com>
2024-09-25 13:21:02 +0300

    Minor updates to CSS in general

--- cmps/NotFound.jsx
+++ cmps/NotFound.jsx
@@ -4,13 +4,12 @@ export function NotFound() {
 
     return (
         <section className="not-found-container">
-
             <section className="not-found">
                 <h1>Oh NO!</h1>
                 <h2>Cannot found route!</h2>
             </section>
+            
             <Link to="/">Back</Link>
         </section>
-
     )
 }
