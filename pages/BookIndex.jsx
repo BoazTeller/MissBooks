9c4d9b9983120986f1c55a80e948b8f978deeae0
BillOfBalrog <boaz.teller@gmail.com>
2024-09-25 13:27:20 +0300

    Refactor BookIndex page to use Swal for prompts and improve page style

--- pages/BookIndex.jsx
+++ pages/BookIndex.jsx
@@ -1,4 +1,7 @@
 import { bookService } from "../services/book.service.js"
+import { showSuccessMsg, showErrorMsg } from "../services/event-bus.service.js"
+import Swal from "../lib/sweetalert2.all.js"
+
 import { BookList } from "../cmps/BookList.jsx"
 import { BookFilter } from "../cmps/BookFilter.jsx"
 
@@ -19,6 +22,7 @@ export function BookIndex() {
             .then(books => setBooks(books))
             .catch(err => {
                 console.error('Failed to query books:', err)
+                showErrorMsg('Could not load books.')
             })
     }
 
@@ -27,51 +31,62 @@ export function BookIndex() {
     }
     
     function onRemoveBook(bookId) {
-        const isConfirmed = prompt('Are you sure?')
-        if (!isConfirmed) return
-
-        bookService.remove(bookId)
-            .then(() => {
-                setBooks(prevBooks => 
-                    prevBooks.filter(book => book.id !== bookId)
-                )
+        Swal.fire(getSwalOpts())
+            .then(response => {
+                if (response.isConfirmed) {
+                    bookService.remove(bookId)
+                        .then(() => {
+                            setBooks(prevBooks =>
+                                prevBooks.filter(book => book.id !== bookId)
+                            )
+                            showSuccessMsg('Book removed successfully!')
+                        })
+                        .catch(err => {
+                            console.error('Had issues with removing book:', err)
+                            showErrorMsg('Could not remove the book.')
+                        })
+                }
             })
-            .catch(err => {
-                console.error('Had issues with removing book:', err)
-            })
-    }    
-
-    function onAddGoogleBook() {
-        bookService.addGoogleBook(book)
-            .then(addedBook => {
-                console.log(addedBook)
-                // show success msg
-            })
-            .catch(err => {
-                console.error('Had issues with adding book from Google:', err)
-        })
     }
     
-    if (!books) return <img src="/assets/img/books-loader.gif" />
+    //TODO: Refactor -> find more elegant solution
+    function getSwalOpts() {
+        return {
+            text: 'Do you want to remove this book?',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes',
+            cancelButtonText: 'No',
+            icon: null,  
+            title: '',
+            width: '300px'
+        }
+    }
 
+    if (!books) return <img className="img-loader" src="/assets/img/books-loader.gif" />
 
     return (
         <section className="book-index">
+            <section className="add-book-btn-container">
+                <Link to="/book/edit">
+                    <button className="btn-cta">Add a new book</button>
+                </Link>
+                <Link to="/book/add">
+                    <button className="btn-cta">Add from Google Books</button>
+                </Link>
+            </section>
+
+
             <BookFilter
                 filterBy={filterBy}
                 handleFilterChange={handleFilterChange}
             />
 
-            <Link to="/book/edit"><button>Add a new book</button></Link>
-            <Link to="/book/add"><button>Add from Google Books</button></Link>
-            
-            {books.length 
-                ? <BookList
-                    books={books}
-                    onRemoveBook={onRemoveBook}
-                />
-                : <h1>No books found...</h1>
-            }
+            <BookList
+                books={books}
+                onRemoveBook={onRemoveBook}
+            />
         </section>
     )
 }
