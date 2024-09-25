e2018b07ef48d3941e49355b2c14cd4997faf301
BillOfBalrog <boaz.teller@gmail.com>
2024-09-25 13:15:07 +0300

    Refactor HomePage and improve style

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,8 +1,18 @@
+const { Link } = ReactRouterDOM
+
 export function HomePage() {
- 
+
     return (
-        <section>
-            <h2>Home Sweet Home</h2>
+        <section className="home-page">
+            <div className="welcome-banner-wrapper">
+                <h1>Welcome to MissBooks!</h1>
+            </div>
+
+            <p>Your one-stop shop for all things books.</p> 
+
+            <Link to="/book">
+                <img className="img-link img-hero" src="./assets/img/hero.png" alt="Bookshelf" />
+            </Link>
         </section>
     )
 }
